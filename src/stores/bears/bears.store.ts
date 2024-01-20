a13abe79ef6611ca126e8063f2ffab8f680d8b09
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-01-19 22:53:47 -0300

    Created 3 components of bears

--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -5,12 +5,21 @@ interface BearState {
   polarBears: number;
   pandaBears: number;
   increaseBlackBear: (by: number) => void;
+  increasePolarBear: (by: number) => void;
+  increasePandaBears: (by: number) => void;
 }
 
 export const useBearStore = create<BearState>((set) => ({
   blackBears: 10,
   polarBears: 5,
   pandaBears: 1,
+
   increaseBlackBear: (by: number) =>
     set((state) => ({ blackBears: state.blackBears + by })),
+
+  increasePolarBear: (by: number) =>
+    set((state) => ({ polarBears: state.polarBears + by })),
+
+  increasePandaBears: (by: number) =>
+    set((state) => ({ pandaBears: state.pandaBears + by })),
 }));
