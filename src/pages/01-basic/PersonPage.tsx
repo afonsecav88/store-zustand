ce4253ecc6f911ed35b7361c02ae15320b2fa083
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-01-21 15:47:06 -0300

    Created person state

--- src/pages/01-basic/PersonPage.tsx
+++ src/pages/01-basic/PersonPage.tsx
@@ -1,12 +1,17 @@
 import { WhiteCard } from '../../components';
-
-
+import { userPersonStore } from '../../stores';
 
 export const PersonPage = () => {
+  const firstName = userPersonStore((state) => state.firstName);
+  const lastName = userPersonStore((state) => state.lastName);
+  const setFirstName = userPersonStore((state) => state.setFirstName);
+  const setLastName = userPersonStore((state) => state.setLastName);
   return (
     <>
       <h1>Persona</h1>
-      <p>Información que se compartirá a otro store, Session Storage y Firebase</p>
+      <p>
+        Información que se compartirá a otro store, Session Storage y Firebase
+      </p>
       <hr />
 
       <WhiteCard className="flex items-center justify-center p-12">
@@ -15,43 +20,45 @@ export const PersonPage = () => {
             <div className="-mx-3 flex flex-wrap">
               <div className="w-full px-3 sm:w-1/2">
                 <div className="mb-5">
-                  <label
-                    className="mb-3 block text-base font-medium text-[#07074D]"
-                  >
+                  <label className="mb-3 block text-base font-medium text-[#07074D]">
                     Primer Nombre
                   </label>
                   <input
                     type="text"
                     name="firstName"
+                    value={firstName}
                     id="firstName"
                     placeholder="Primer Nombre"
+                    onChange={(e) => setFirstName(e.target.value)}
                   />
                 </div>
               </div>
               <div className="w-full px-3 sm:w-1/2">
                 <div className="mb-5">
-                  <label
-                    className="mb-3 block text-base font-medium text-[#07074D]"
-                  >
+                  <label className="mb-3 block text-base font-medium text-[#07074D]">
                     Apellido
                   </label>
                   <input
                     type="text"
                     name="lastName"
+                    value={lastName}
                     id="lastName"
                     placeholder="Apellido"
+                    onChange={(e) => setLastName(e.target.value)}
                   />
                 </div>
               </div>
             </div>
-  
+
             <pre className="bg-gray-200 p-5 rounded-[20px]">
-              {
-                JSON.stringify({
-                  firstName: '',
-                  lastName: ''
-                }, null, 2)
-              }
+              {JSON.stringify(
+                {
+                  firstName,
+                  lastName,
+                },
+                null,
+                2
+              )}
             </pre>
           </form>
         </div>
